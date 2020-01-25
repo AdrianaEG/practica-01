e9eb191df8d592b037e9d8b856e8c0b7d78a11e4
AdrianaEG <adriana.ngc438@gmail.com>
2020-01-25 19:28:18 -0300

    lo hago responsive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const $contenedor = document.querySelector('#contenedor');
-
 const $botonMenu = document.querySelector('#boton-menu');
+let elementos = document.querySelectorAll('.menu-lateral a span');
 
 $botonMenu.addEventListener('click', () => {
     $contenedor.classList.toggle('active');
-    let elementos = document.querySelectorAll('.menu-lateral a span');
+    
     if ($contenedor.className !== 'contenedor active') {
         elementos.forEach(function (elemento) {
             elemento.style.display = 'none';
@@ -14,4 +14,26 @@ $botonMenu.addEventListener('click', () => {
             elemento.style.display = 'inline';
         });
     }
+});
+
+const comprobarAncho = () => { //funcion de tipo flecha
+    if(window.innerWidth <= 768){
+        $contenedor.classList.remove('active');
+        elementos.forEach(function (elemento) {
+            elemento.style.display = 'none';
+        });
+    }
+    else{
+        $contenedor.classList.add('active');
+        elementos.forEach(function (elemento) {
+            elemento.style.display = 'inline';
+        });
+    }
+}
+
+comprobarAncho();
+
+
+window.addEventListener('resize', () =>{
+    comprobarAncho();
 });
